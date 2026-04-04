956ba8a7474c581150575be7011f8273b27c8baa
mmmorg15 <mmmorg15@byu.edu>
2026-04-03 21:59:03 -0600

    front

--- frontend/src/api/apiConfig.ts
+++ frontend/src/api/apiConfig.ts
@@ -1,6 +1,6 @@
 const LOCAL_API_URL = "https://localhost:5000/api/book";
 const PRODUCTION_API_URL =
-    "https://book-morgan-backend-c2h0azdzascva2cg.francecentral-01.azurewebsites.net";
+    "https://book-morgan-backend-c2h0azdzascva2cg.francecentral-01.azurewebsites.net/api/book";
 
 const defaultApiUrl =
     window.location.hostname === "localhost" ? LOCAL_API_URL : PRODUCTION_API_URL;
